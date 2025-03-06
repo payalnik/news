dff73a6e300cdcd3bfb28925a9baa31d0032b199
Alex Linux <payalnik@gmail.com>
2025-03-06 17:52:23 +0000

    Settings

--- news_updater/news_updater/settings.py
+++ news_updater/news_updater/settings.py
@@ -16,6 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = 'True'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','news.alexilin.com']
 
